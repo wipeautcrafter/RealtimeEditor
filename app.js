03746be217c041f6c9bcc93a3c10f1b8e621c2b1
wipeautcrafter <wipeautcrafter@gmail.com>
2017-09-20 20:13:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
 res.sendFile(__dirname + '/src/index.html');
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+http.listen(port, "0.0.0.0", function(){
+  console.log('listening on *:'+port);
 });
 
 io.on('connection', function(socket) {
